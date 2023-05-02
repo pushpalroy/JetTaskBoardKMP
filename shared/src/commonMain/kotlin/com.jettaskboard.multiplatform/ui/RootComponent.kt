f2b877db01b6af00d83736f819a34a75db6ec6e0
Pushpal Roy <pushpal.roy@mutualmobile.com>
2023-05-03 00:57:43 +0530

    Add dashboard to board navigation

--- shared/src/commonMain/kotlin/com.jettaskboard.multiplatform/ui/RootComponent.kt
+++ shared/src/commonMain/kotlin/com.jettaskboard.multiplatform/ui/RootComponent.kt
@@ -3,6 +3,8 @@ package com.jettaskboard.multiplatform.ui
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.essenty.parcelable.Parcelable
 import com.arkivanov.essenty.parcelable.Parcelize
 import com.jettaskboard.multiplatform.ui.screens.board.TaskBoardRoute
@@ -22,8 +24,16 @@ fun RootComponent() {
             animation = stackAnimation(animator = slide()),
             content = { screen ->
                 when (screen) {
-                    is RootStateModel.Dashboard -> DashboardRoute()
-                    is RootStateModel.TaskBoard -> TaskBoardRoute()
+                    is RootStateModel.Dashboard -> DashboardRoute(
+                        navigateToTaskBoard = {
+                            router.push(RootStateModel.TaskBoard)
+                        }
+                    )
+                    is RootStateModel.TaskBoard -> TaskBoardRoute(
+                        onBackClick = {
+                            router.pop()
+                        }
+                    )
                     else -> {}
                 }
             }
