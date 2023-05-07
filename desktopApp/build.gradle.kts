a307e3a56309fdfdef7425adbcb1285170a36d2c
Pushpal Roy <pushpalroy2007@gmail.com>
2023-05-08 01:47:18 +0530

    add separate desktopApp module

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    jvm {}
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation(project(":shared"))
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb
+            )
+            packageName = "com.jettaskboard.mutiplatform"
+            packageVersion = "1.0.0"
+        }
+    }
+}
--- shared/src/desktopMain/kotlin/com/jettaskboard/multiplatform/main.desktop.kt
+++ shared/src/desktopMain/kotlin/com/jettaskboard/multiplatform/main.desktop.kt
@@ -1,29 +1,19 @@
 package com.jettaskboard.multiplatform
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.jettaskboard.multiplatform.di.initKoin
 import com.jettaskboard.multiplatform.util.krouter.LocalComponentContext
 
-fun main() = application {
-
+@Composable
+fun MainView(){
     initKoin()
     val lifecycle = LifecycleRegistry()
-
     val rootComponentContext = DefaultComponentContext(lifecycle = lifecycle)
-    Window(
-        title = "JetTaskBoardKMP",
-        state = rememberWindowState(width = 1200.dp, height = 800.dp),
-        onCloseRequest = ::exitApplication,
-    ) {
-        CompositionLocalProvider(LocalComponentContext provides rootComponentContext) {
-            App(isExpandedScreen = true)
-        }
+    CompositionLocalProvider(LocalComponentContext provides rootComponentContext) {
+        App(isExpandedScreen = true)
     }
 }
 
